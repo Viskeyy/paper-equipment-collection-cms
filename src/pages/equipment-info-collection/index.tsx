6d72b0103f39ce2d3452ab6a2b921ca7d183659f
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2026-05-25 11:33:02 +0800

    feat(equipment-info-collection): use env ingest url

--- src/pages/equipment-info-collection/index.tsx
+++ src/pages/equipment-info-collection/index.tsx
@@ -112,7 +112,12 @@ export const EquipmentInfoCollectionForm = () => {
         }
 
         try {
-            const res = await fetch('http://156.245.146.39:38000/ingest', {
+            const url = import.meta.env.VITE_API_URL;
+            if (!url) {
+                throw new Error('API URL is not defined');
+            }
+
+            const res = await fetch(url, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(bodyData),
